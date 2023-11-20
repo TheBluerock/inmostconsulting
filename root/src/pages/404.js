ea18b5fa9ab3a3084400bebdac85e03d6c8dadea
TheBluerock <blu3rock@gmail.com>
2023-11-21 00:12:07 +0100

    404

--- root/src/pages/404.js
+++ root/src/pages/404.js
@@ -2,16 +2,35 @@ import React, { useEffect } from 'react';
 import NotFound from '@components/not-found';
 import TitleMarquee from '@components/marquee-title';
 import { useTheme } from '@emotion/react';
+import styled from "@emotion/styled";
 
 const NotFoundPage = () => {
   const theme = useTheme();
 
   return (
     <>
-      <TitleMarquee text={'Qui non vi è nulla'} size={theme.typography.h4} />
       <NotFound />
     </>
   );
 };
 
 export default NotFoundPage;
+
+const MarqueeText = styled.a`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${({ theme }) => theme.typography.h4.desktop};
+  font-family: ${({ theme }) => theme.fonts.serif};
+  text-transform: uppercase;
+  text-decoration: none;
+  margin: 24px 12px;
+  @media ${({ theme }) => theme.device.large} {
+    font-size: ${({ theme }) => theme.typography.h4.tablet};
+  }
+  @media ${({ theme }) => theme.device.small} {
+    font-size: ${({ theme }) => theme.typography.h3.mobile};
+  }
+`;
+
+const Wrapper = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.colors.primary};
+`;
